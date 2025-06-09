eeff9677f0eb91bc69521480f0425ac300abdeab
CinquinAndy <cinquin.andy@gmail.com>
2025-06-10 00:18:55 +0200

    fix: seo fix (block bots)

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,24 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	async headers() {
+		return [
+			{
+				// Apply to all routes
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'X-Robots-Tag',
+						value: 'noindex, nofollow, noarchive, nosnippet, noimageindex, nocache',
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff',
+					},
+				],
+			},
+		]
+	},
+}
 
 export default nextConfig
