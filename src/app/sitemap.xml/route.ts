e9698efff347f5c845a480abe7649c4d200cb4b8
CinquinAndy <cinquin.andy@gmail.com>
2025-06-10 00:18:55 +0200

    fix: seo fix (block bots)

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+
+export function GET() {
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <!-- Web site in development - No URL to index -->
+</urlset>`
+
+	return new NextResponse(sitemap, {
+		headers: {
+			'Content-Type': 'application/xml',
+			'X-Robots-Tag': 'noindex',
+		},
+	})
+}
