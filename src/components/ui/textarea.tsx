cbbb4225a8d438c8521d1814fd54321f680dc53e
Breval <breval2000@live.fr>
2025-06-17 18:31:38 +0200

    feat: enhance input and textarea components with hover effects and add label component
    - Refactored Input component to include a radial gradient hover effect using motion.
    - Introduced a new Label component for better accessibility and styling.
    - Created a SignupFormDemo component to demonstrate the usage of Input and Label components.
    - Updated Textarea component to include similar hover effects as the Input component.

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,18 +1,49 @@
+'use client'
+
+import { motion, useMotionTemplate, useMotionValue } from 'motion/react'
 import React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>(
 	({ className, ...props }, ref) => {
+		const radius = 100 // change this to increase the radius of the hover effect
+		const [visible, setVisible] = React.useState(false)
+
+		let mouseX = useMotionValue(0)
+		let mouseY = useMotionValue(0)
+
+		function handleMouseMove({ currentTarget, clientY, clientX }: React.MouseEvent<HTMLDivElement>) {
+			let { top, left } = currentTarget.getBoundingClientRect()
+
+			mouseX.set(clientX - left)
+			mouseY.set(clientY - top)
+		}
 		return (
-			<textarea
-				className={cn(
-					'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-					className
-				)}
-				ref={ref}
-				{...props}
-			/>
+			<motion.div
+				className="group/textarea rounded-lg p-[2px] transition duration-300"
+				onMouseEnter={() => setVisible(true)}
+				onMouseLeave={() => setVisible(false)}
+				onMouseMove={handleMouseMove}
+				style={{
+					background: useMotionTemplate`
+        radial-gradient(
+          ${visible ? radius + 'px' : '0px'} circle at ${mouseX}px ${mouseY}px,
+          #8b5cf6,
+          transparent 80%
+        )
+      `,
+				}}
+			>
+				<textarea
+					className={cn(
+						`shadow-input dark:placeholder-text-neutral-600 flex min-h-[80px] w-full rounded-md border-none bg-gray-50 px-3 py-2 text-sm text-black transition duration-400 group-hover/textarea:shadow-none placeholder:text-neutral-400 focus-visible:ring-[2px] focus-visible:ring-neutral-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:bg-zinc-800 dark:text-white dark:shadow-[0px_0px_1px_1px_#404040] dark:focus-visible:ring-neutral-600`,
+						className
+					)}
+					ref={ref}
+					{...props}
+				/>
+			</motion.div>
 		)
 	}
 )
