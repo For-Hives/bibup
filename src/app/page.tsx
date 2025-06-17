a3f2edcfe6373c932093837b24c3d20151611553
CinquinAndy <cinquin.andy@gmail.com>
2025-06-17 02:32:28 +0200

    fix(landing): update homepage visuals and event data
    Refactors hero section with background image and gradient overlay for better aesthetics.
    Updates event data with new images, locations, distances, and participant counts for better accuracy and variety.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { HeroAnimation } from '@/components/landing/hero-animation'
@@ -8,10 +9,37 @@ const runs = [
 		originalPrice: 100,
 		event: {
 			participantCount: 10000,
-			name: 'Paris Marathon',
-			location: 'Paris',
+			name: 'Marathon de Nantes',
+			location: 'Nantes',
+			image: '/bib-red.png',
 			distanceUnit: 'km',
-			distance: 100,
+			distance: 42,
+			date: new Date(),
+		},
+	},
+	{
+		price: 440,
+		originalPrice: 500,
+		event: {
+			participantCount: 64000,
+			name: 'Ironman Nice',
+			location: 'Nice',
+			image: '/bib-green.png',
+			distanceUnit: 'km',
+			distance: 226,
+			date: new Date(),
+		},
+	},
+	{
+		price: 9.99,
+		originalPrice: 15,
+		event: {
+			participantCount: 2630,
+			name: 'Semi-marathon de Thonon',
+			location: 'Thonon-les-bains',
+			image: '/bib-pink.png',
+			distanceUnit: 'km',
+			distance: 21,
 			date: new Date(),
 		},
 	},
@@ -20,34 +48,24 @@ const runs = [
 		originalPrice: 100,
 		event: {
 			participantCount: 10000,
-			name: 'Paris Marathon',
+			name: 'Marathon de Paris',
 			location: 'Paris',
+			image: '/bib-blue.png',
 			distanceUnit: 'km',
-			distance: 100,
+			distance: 42,
 			date: new Date(),
 		},
 	},
 	{
-		price: 79.99,
-		originalPrice: 100,
+		price: 900,
+		originalPrice: 600,
 		event: {
-			participantCount: 10000,
-			name: 'Paris Marathon',
-			location: 'Paris',
+			participantCount: 9000,
+			name: 'Ultra Trail du Mont Blanc',
+			location: 'Chamonix',
+			image: '/bib-orange.png',
 			distanceUnit: 'km',
-			distance: 100,
-			date: new Date(),
-		},
-	},
-	{
-		price: 79.99,
-		originalPrice: 100,
-		event: {
-			participantCount: 10000,
-			name: 'Paris Marathon',
-			location: 'Paris',
-			distanceUnit: 'km',
-			distance: 100,
+			distance: 170,
 			date: new Date(),
 		},
 	},
@@ -57,8 +75,16 @@ export default function Home() {
 	return (
 		<div className="">
 			{/* Hero Section */}
-			<section className="pt-32">
-				<div className="mx-auto max-w-7xl">
+			<section className="relative pt-32">
+				<Image
+					alt="template-run"
+					className="-z-10 -scale-x-100 overflow-hidden object-cover opacity-30"
+					fill
+					sizes="100vw"
+					src={'/TRV_Ultra_AlexDiaz.jpg'}
+				/>
+				<div className="from-background/100 to-background/100 absolute inset-0 -z-10 bg-gradient-to-r via-zinc-900/60"></div>
+				<div className="z-20 mx-auto max-w-7xl">
 					<div className="grid grid-cols-12 gap-4">
 						<div className="col-span-5 flex flex-col justify-center gap-6 pb-32">
 							<h1 className="text-foreground text-5xl font-bold tracking-tight">
