02d95e40edf99b8e152076eb7c5db511018cb954
CinquinAndy <cinquin.andy@gmail.com>
2025-06-21 04:18:49 +0200

    fix(models): add timestamp fields to user and bib interfaces

--- src/models/bib.model.ts
+++ src/models/bib.model.ts
@@ -4,20 +4,24 @@ import { User } from './user.model'
 export interface Bib {
 	buyerUserId?: User['id']
 
+	// PocketBase fields
+	created: string
 	eventId: Event['id']
-	id: string
 
+	id: string
 	listed: 'private' | 'public' | null
 	// values for options defined on the Event
 	optionValues: Record<string, string>
 	originalPrice?: number
-	price: number
 
+	price: number
 	privateListingToken?: string
 	registrationNumber: string
+
 	sellerUserId: User['id']
 
 	status: 'available' | 'expired' | 'sold' | 'validation_failed' | 'withdrawn'
 
+	updated: string
 	validated: boolean
 }
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,9 +1,13 @@
 export interface User {
 	clerkId: string
+	// PocketBase fields
+	created: string
 	createdAt: Date
 	email: string
 	firstName: string
 	id: string
 	lastName: string
+
 	role: 'admin' | 'user'
+	updated: string
 }
