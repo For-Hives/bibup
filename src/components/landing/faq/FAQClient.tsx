356bbe4f18f546d1a6c44c7548b075138237cc9a
Breval <breval2000@live.fr>
2025-06-23 17:57:58 +0200

    feat: enhance FAQ component by integrating locale support and improving translation handling

--- src/components/landing/faq/FAQClient.tsx
+++ src/components/landing/faq/FAQClient.tsx
@@ -2,33 +2,22 @@
 
 import { PhoneCall } from 'lucide-react'
 
+import Link from 'next/link'
+
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
+import { Locale } from '@/lib/i18n-config'
 
 interface FAQClientProps {
-	translations: {
-		faq: {
-			badge: string
-			contactButton: string
-			description: string
-			items: FaqItem[]
-			support: {
-				buttonUrl: string
-			}
-			title: string
-		}
-	}
+	locale: Locale
 }
 
-interface FaqItem {
-	answer: string
-	id: string
-	question: string
-}
+import { getTranslations } from '@/lib/getDictionary'
 
-export default function FAQClient({ translations }: FAQClientProps) {
-	const { faq } = translations
+import Translations from './locales.json'
+export default function FAQClient({ locale }: FAQClientProps) {
+	const t = getTranslations(locale, Translations)
 
 	return (
 		<div className="w-full px-4 py-12 md:py-24 xl:px-0">
@@ -38,19 +27,19 @@ export default function FAQClient({ translations }: FAQClientProps) {
 					<div className="flex flex-col gap-10">
 						<div className="flex flex-col gap-4">
 							<div>
-								<Badge variant="outline">{faq.badge}</Badge>
+								<Badge variant="outline">{t.faq.badge}</Badge>
 							</div>
 							<div className="flex flex-col gap-2">
-								<h4 className="font-regular max-w-xl text-left text-3xl tracking-tighter md:text-5xl">{faq.title}</h4>
+								<h4 className="font-regular max-w-xl text-left text-3xl tracking-tighter md:text-5xl">{t.faq.title}</h4>
 								<p className="text-muted-foreground max-w-xl text-left leading-relaxed tracking-tight lg:max-w-lg lg:text-lg">
-									{faq.description}
+									{t.faq.description}
 								</p>
 							</div>
 							<div className="">
 								<Button asChild className="gap-4" variant="outline">
-									<a href={faq.support.buttonUrl}>
-										{faq.contactButton} <PhoneCall className="h-4 w-4" />
-									</a>
+									<Link href={t.faq.support.buttonUrl}>
+										{t.faq.contactButton} <PhoneCall className="h-4 w-4" />
+									</Link>
 								</Button>
 							</div>
 						</div>
@@ -58,7 +47,7 @@ export default function FAQClient({ translations }: FAQClientProps) {
 
 					{/* Right Column - Accordion */}
 					<Accordion className="w-full" collapsible type="single">
-						{faq.items.map(item => (
+						{t.faq.items.map(item => (
 							<AccordionItem key={item.id} value={item.id}>
 								<AccordionTrigger className="text-left">{item.question}</AccordionTrigger>
 								<AccordionContent className="text-muted-foreground">{item.answer}</AccordionContent>
